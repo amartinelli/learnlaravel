711715ad0e01e6e52ef56a267b1e49c061dbaaa5
Andre Agunzi <aagunzi@gmail.com>
2016-02-22 21:40:48 -0300

    blog implements

---
+++ blog/app/Http/Controllers/BlogController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class BlogController extends Controller
+{
+    //
+
+
+	public function index()
+	{
+		return view('blog/index');
+	}
+
+	public function blog()
+	{
+
+		$posts = [
+			0 => array('user1','Texto de post 1', 'Data do post'),
+			1 => array('user2','Texto de post 2', 'Data do post'),
+                        2 => array('user1','Texto de post 3', 'Data do post'),
+                        3 => array('user2','Texto de post 4', 'Data do post'),
+                        4 => array('user2','Texto de post 5', 'Data do post'),
+                        5 => array('user1','Texto de post 6', 'Data do post'),
+
+		];
+
+		return view('blog/posts', compact('posts'));
+	}
+
+
+}
---
+++ blog/resources/views/blog/index.blade.php
@@ -0,0 +1,13 @@
+	
+	@extends('template')
+
+	@section('title')
+
+		Sistema de Blog com Laravel
+	@stop
+
+	@section('content')
+
+	<h1>Sistema de Blog com Laravel</h1>
+
+	@stop
